7714e8782d1bca602a4b1dd0c2b52bb4e9febbc2
ForumPlayer <fp@forumplayer.dev>
2021-06-19 03:25:38 -0300

    Fix init.sh to handle all known bugs

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [ ! -n "$(pidof mpd)" ]; then mpd; fi
+if [ ! -n "$(mpc queue)" ]; then echo -e "Queue is Empty! \nOpening default shell."; $SHELL; exit; fi
 bin="$(realpath "$0")"
 ${bin%/*}/runtime.sh
