44a22346e812d6db76c78e046cc31972664db306
ForumPlayer <fp@forumplayer.dev>
2021-06-20 05:20:20 -0300

    Fix the non-playing bug at starting container

--- bin/init.sh
+++ bin/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ ! -n "$(pidof mpd)" ]; then mpd; fi
 if [ ! -n "$(mpc queue)" ]; then echo -e "Queue is Empty! \nOpening default shell."; $SHELL; exit; fi
+if [ ! -n "$(mpc current)" ]; then mpc play; fi
 bin="$(realpath "$0")"
 ${bin%/*}/runtime.sh
